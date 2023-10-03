a5f3ad1a1a059f6afffa24c87e274f092aca0039
Ojochogwu Dickson <07afrik@gmail.com>
2023-10-03 12:18:01 +0100

    initial-commit

--- deploy/01-deploy-primary-token.ts
+++ deploy/01-deploy-primary-token.ts
@@ -1,10 +1,30 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { ethers } from "ethers"
 
 const deployGovernanceToken: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
 ){
-    console.log("Hello");
+    const {getNamedAccounts, deployments, network} = hre;
+    const {deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    log("Deploying Governance Token...")
+
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+        // wait confirmations
+    })
+    //verify to check if its the development chain
+    log(`Deployed governance token to address ${governanceToken.address}`)
+};
+
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+    const governanceToken = await ethers.getAddress("GovernanceToken");
+    const tx = await governanceToken.delegate(delegatedAccount);
+    await tx.wait(1);
+    console.log(`Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`);
 }
 
-
+export default deployGovernanceToken
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/types";
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.19",
-};
+//module.exports = {
+ // solidity: "0.8.19",
+//};
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks:{
+    hardhat:{
+      chainId: 30080, //31337
+    },
+    localhost:{
+      chainId: 30080, //31337
+    }
+  },
+    solidity: "0.8.19",
+    namedAccounts: {
+      deployer: {
+        defaults: 0
+      }
+    }
+  }
+
+  export default config
